f0365645545f0130cc0556836e9edf5b24fab094
sayildar <sayildar17@gmail.com>
2024-06-03 01:49:26 +0500

    started project

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomSimpleUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, verbose_name=_('Номер телефона'), unique=True)
+
+
